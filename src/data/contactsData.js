c1e49b784ce00436b34edd77d31695bad2c494b1
fares.talbi <farestalbi@takiacademyteam.com>
2024-09-04 10:51:11 +0100

    portfolio v1

---
+++ src/data/contactsData.js
@@ -0,0 +1,7 @@
+export const contactsData = {
+    email: 'talbii.fares@gmail.com',
+    phone: '+216 25912040',
+    address: 'Tunis',
+
+    sheetAPI: ''
+}
---
+++ src/data/educationData.js
@@ -0,0 +1,16 @@
+export const educationData = [
+  {
+    id: 1,
+    institution: "Higher Institute of Multimedia Arts of Manouba (ISAMM)",
+    course: "Bachelor's Degree in Computer Science",
+    startYear: "2018",
+    endYear: "2021",
+  },
+  {
+    id: 2,
+    institution: "Polytechnic School of Sousse",
+    course: "Engineering Degree in Computer Science",
+    startYear: "2021",
+    endYear: "Present",
+  },
+];
